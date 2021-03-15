bf0fc530d02eabe93c987ea0d9cfe7659fdab67a
bryantmaodesign <bmao.design@gmail.com>
2021-03-15 12:20:06 +0800

    add approach expand/fold

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-const toggleButton = document.getElementsByClassName ('toggle-button')[0]
-const navMainLinks = document.getElementsByClassName ('nav-main__links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navMainLinks.classList.toggle('active')
-})
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
