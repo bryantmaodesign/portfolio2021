822154a4ebd6460342875584629a1469d55922c6
bryantmaodesign <bmao.design@gmail.com>
2021-03-15 23:19:48 +0800

    update footer

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+//back to top button
+
+topbutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
