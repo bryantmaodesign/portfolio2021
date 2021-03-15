3076bff4248c7fb9386e46342766871e12bf9e29
bryantmaodesign <bmao.design@gmail.com>
2021-03-15 12:25:28 +0800

    add comments

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// hanburger menu 
+
 const toggleButton = document.getElementsByClassName ('toggle-button')[0]
 const navMainLinks = document.getElementsByClassName ('nav-main__links')[0]
 
 toggleButton.addEventListener('click', () => {
     navMainLinks.classList.toggle('active')
 })
+
+
+// collapsible approach section
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
